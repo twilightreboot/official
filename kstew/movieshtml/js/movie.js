71cfdeee3c065438cb450df825ea79576bb9ab3b
Saad Dar <167533717+iamsaaddar@users.noreply.github.com>
2025-01-11 11:49:31 +0500

    om

--- kstew/movieshtml/js/movie.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.movie-list li').forEach(item => {
-    item.addEventListener('click', () => {
-        item.classList.add('clicked');
-        setTimeout(() => {
-            item.classList.remove('clicked');
-        }, 300); // Duration of the clickEffect animation
-    });
-});
